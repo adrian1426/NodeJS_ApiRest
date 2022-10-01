81f5ddbd703df039fb730b4acdb6dd34934ffa41
ADRIAN HERNANDEZ <adrianhh1426@gmail.com>
2022-10-01 15:55:32 -0500

    Proyecto base

---
+++ app.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+
+const PORT = process.env.PORT || 8080;
+const app = express();
+
+app.use(cors());
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
