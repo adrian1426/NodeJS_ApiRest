fc1fb565187bf018ad1b99df7cf5936ea2c43220
ADRIAN HERNANDEZ <adrianhh1426@gmail.com>
2022-10-07 21:26:33 -0500

    haciendo login

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -2,33 +2,63 @@ const { matchedData } = require('express-validator');
 const { userModel } = require('../models/mongo');
 const { encrypt, validate } = require('../utils/handlePassword');
 const { signToken } = require('../utils/handleJWT');
+const { handleHttpError } = require('../utils/handlerError');
 
 const addUser = async (req, res) => {
-  req = matchedData(req);
-  const pwdEncripted = await encrypt(req.password);
+  try {
+    req = matchedData(req);
+    const pwdEncripted = await encrypt(req.password);
 
-  const user = {
-    ...req,
-    password: pwdEncripted
-  };
+    const user = {
+      ...req,
+      password: pwdEncripted
+    };
 
-  const resBdModel = await userModel.create(user);
-  resBdModel.set('password', undefined, { strict: false });
+    const resBdModel = await userModel.create(user);
+    resBdModel.set('password', undefined, { strict: false });
 
-  const userToken = await signToken(user);
+    const userToken = await signToken(user);
 
-  const response = {
-    token: userToken,
-    user: resBdModel
-  };
+    const response = {
+      token: userToken,
+      user: resBdModel
+    };
 
-  res.send({ message: 'register', data: response });
+    res.send({ message: 'register', data: response });
+  } catch (error) {
+    handleHttpError(res, `Error: ${error}`, 500);
+  }
 };
 
-const login = (req, res) => {
-  req = matchedData(req);
+const login = async (req, res) => {
+  try {
+    req = matchedData(req);
+    const userFound = await userModel.findOne({ email: req.email })
+      .select('password name email role');
 
-  res.send({ message: 'login', received: req });
+    if (!userFound) {
+      handleHttpError(res, 'Usuario No encontrado', 404);
+      return;
+    }
+
+    const idValidPwd = await validate(req.password, userFound.password);
+
+    if (!idValidPwd) {
+      handleHttpError(res, `No estás autorizado`, 401);
+      return;
+    }
+
+    const token = await signToken(userFound);
+
+    const response = {
+      token,
+      user: userFound
+    };
+
+    res.send({ message: 'login', data: response });
+  } catch (error) {
+    handleHttpError(res, `Error: ${error}`, 500);
+  }
 };
 
 module.exports = {
