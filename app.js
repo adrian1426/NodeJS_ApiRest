63224ea8fe9b62e67614f32df9ca251cc05d028a
ADRIAN HERNANDEZ <adrianhh1426@gmail.com>
2022-10-08 14:42:43 -0500

    Enviando errores a un canla se slack

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ require('dotenv').config();
 
 const express = require('express');
 const cors = require('cors');
+const morganBody = require('morgan-body');
 const dbMongoConnect = require('./src/config/mongoConfig');
 const routes = require('./src/routes');
+const { loggerStream } = require('./src/utils/handleMorganIntercep');
 
 const PORT = process.env.PORT || 8080;
 const app = express();
@@ -11,6 +13,15 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.static('src/storage'));
+
+morganBody(app, {
+  noColors: true,
+  stream: loggerStream,
+  skip: function (req, res) {
+    return res.statusCode < 400;
+  }
+});
+
 app.use(routes);
 
 app.listen(PORT, () => {
