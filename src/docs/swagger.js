776ece112fcfd8c6ede7b912e052b2eac254ac9f
ADRIAN HERNANDEZ <adrianhh1426@gmail.com>
2022-10-08 22:44:35 -0500

    creando schema de swagger

--- src/docs/swagger.js
+++ src/docs/swagger.js
@@ -8,7 +8,54 @@ const swaggerDefinition = {
   },
   servers: [
     { url: 'http://localhost:3000/v1/api' }
-  ]
+  ],
+  components: {
+    schemas: {
+      track: {
+        type: 'object',
+        required: ['name', 'album'],
+        properties: {
+          name: {
+            type: 'string'
+          },
+          album: {
+            type: 'string'
+          },
+          cover: {
+            type: 'string'
+          },
+          artista: {
+            type: 'object',
+            properties: {
+              name: {
+                type: 'string'
+              },
+              nickname: {
+                type: 'string'
+              },
+              nationality: {
+                type: 'string'
+              }
+            }
+          },
+          duration: {
+            type: 'object',
+            start: {
+              name: {
+                type: 'integer'
+              },
+              end: {
+                type: 'integer'
+              }
+            }
+          },
+          mediaId: {
+            type: 'string'
+          }
+        }
+      }
+    }
+  }
 };
 
 const options = {
